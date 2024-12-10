91f1477554dee625ef752b4892c4299039e6ef6e
JerePalmu <jerepalmu03@gmail.com>
2024-12-10 19:08:18 +0200

    fullscreen korjattu ja muut viimeistelyt

--- settings.gd
+++ settings.gd
@@ -5,12 +5,9 @@ func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0,value)
 
 
-@onready var mute_checkbox = $MuteCheckbox  # Path to your mute checkbox node
-
-# This function is called when the checkbox is toggled
 func _on_mute_toggled(toggled_on: bool) -> void:
-	AudioManager.is_muted = toggled_on
-	AudioManager.update_audio_state()  # Apply the mute/unmute effect globally
+	AudioServer.set_bus_mute(0, toggled_on) 
+
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://menu.tscn")
